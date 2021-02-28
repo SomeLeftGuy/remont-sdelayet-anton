f10f2902b174a1094c7e6cc0704a47fefd1e6b8c
SomeLeftGuy <gchebodang@gmail.com>
2021-02-28 11:47:02 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.photos').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        adaptiveHeight: true
+    });
+})
